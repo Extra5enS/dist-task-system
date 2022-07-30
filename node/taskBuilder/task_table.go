75337c9a208bda7ccb5ddd75a666363b2f2010bf
extra5ens <molotkov.md@phystech.edu>
2022-07-30 21:38:54 +0300

    Add Type of task base of execution

--- node/taskBuilder/task_table.go
+++ node/taskBuilder/task_table.go
@@ -1,5 +1,12 @@
 package taskBuilder
 
+type TaskType string
+
+const (
+	ExtTaskType = "ExtTaskType" // this task should be with with exect
+	IntTaskType = "IntTaskType" // this task use golang func
+)
+
 // numder of task at a week
 type TaskId uint64
 
@@ -11,18 +18,17 @@ type TaskOut struct {
 type Task struct {
 	TaskName string
 	TaskId   TaskId
-	Keys     []string
 	Args     []string
 }
 
 type TaskInfo struct {
 	Name     string
+	Type     TaskType
 	ArgCount int
 	Keys     []string
 }
 
 var TaskTable = map[string]TaskInfo{
-	"hello": {"hello", 0, []string{}},
-	//"lookup": {"lookup", 1, []string{"lRa"}},
-	//"read":   {"read", 1, []string{""}},
+	"hello": {"hello", IntTaskType, 0, []string{}},
+	"ls":    {"ls", ExtTaskType, 0, []string{}},
 }
