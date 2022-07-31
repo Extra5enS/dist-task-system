8831cce9fa0f5697442b27901dcd524f1443a493
extra5ens <molotkov.md@phystech.edu>
2022-07-31 18:40:57 +0300

    Upgrade sys_task_executor with OS vertions+fix bug

--- node/taskBuilder/sys_task_executor.go
+++ node/taskBuilder/sys_task_executor.go
@@ -2,7 +2,10 @@ package taskBuilder
 
 import (
 	"bytes"
+	"fmt"
 	"os/exec"
+	"runtime"
+	"strings"
 )
 
 type SysTaskExecutor struct {
@@ -10,15 +13,39 @@ type SysTaskExecutor struct {
 }
 
 func (ste SysTaskExecutor) Exec(name string, args []string) (string, error) {
+	os := runtime.GOOS
+	switch os {
+	case "windows":
+		return execWin(name, args)
+	case "darwin":
+		return execMac(name, args)
+	case "linux":
+		return execLin(name, args)
+	default:
+		return "", fmt.Errorf("Not implemented")
+	}
+}
+
+func execWin(name string, args []string) (string, error) {
+	sumCommand := make([]string, 0)
+	sumCommand = append(sumCommand, name)
+	sumCommand = append(sumCommand, args...)
+
 	commandArg := make([]string, 0)
 	commandArg = append(commandArg, "-c")
-	commandArg = append(commandArg, name)
-	commandArg = append(commandArg, args...)
+	commandArg = append(commandArg, strings.Join(sumCommand, " "))
 	cmd := exec.Command("bash", commandArg...)
-	//fmt.Print(name, "\n")
-	//cmd.Stdin = strings.NewReader("some input")
+
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err := cmd.Run()
 	return out.String(), err
+}
+
+func execLin(name string, args []string) (string, error) {
+	return "", fmt.Errorf("Not implemented")
+}
+
+func execMac(name string, args []string) (string, error) {
+	return "", fmt.Errorf("Not implemented")
 }
